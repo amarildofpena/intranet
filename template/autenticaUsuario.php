37dd01333794c438034b5ec862471d6507af1654
amarildofpena <amarildofpena@gmail.com>
2015-01-06 14:36:28 -0300

    01062015ç

--- template/autenticaUsuario.php
+++ template/autenticaUsuario.php
@@ -1,7 +1,8 @@
-﻿<?php
+?<?php
 		include("funcoes.php");
                 if (autentica($_REQUEST['email'], $_REQUEST['senha'])){
                    echo "<meta HTTP-EQUIV='Refresh' CONTENT='0;URL=index.php'>";
+                   
                 }else{
                    echo "<meta HTTP-EQUIV='Refresh' CONTENT='0;URL=cadastroUsuario.php'>";
                 }
